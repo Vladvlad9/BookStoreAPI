8e0ad10cd446cf8fc0ef2425d90f619023e6a13f
Vlad Paulechka <vlad9.ru@gmail.com>
2024-06-18 13:56:49 +0300

    refactor models

--- src/database/models.py
+++ src/database/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, Text, Float, ForeignKey, DateTime, Boolean, SmallInteger, \
-    CheckConstraint, Enum
+from sqlalchemy import Column, Integer, String, Text, Float, ForeignKey, DateTime, Boolean, \
+    CheckConstraint, Enum, SMALLINT
 from sqlalchemy.orm import DeclarativeBase, relationship
 
 from src.enum import UserRole, OrderStatus as order_status
@@ -30,12 +30,16 @@ class Base(DeclarativeBase):
 class BookAuthor(Base):
     __tablename__ = 'books_authors'
 
-    book_id = Column(SmallInteger, ForeignKey("books.id"))
-    author_id = Column(SmallInteger, ForeignKey("authors.id"))
+    id = Column(SMALLINT, primary_key=True)
+
+    book_id = Column(SMALLINT, ForeignKey("books.id"))
+    author_id = Column(SMALLINT, ForeignKey("authors.id"))
 
 
 class BookDiscount(Base):
     __tablename__ = 'book_discounts'
+
+    id = Column(SMALLINT, primary_key=True)
 
     book_id = Column(Integer, ForeignKey('books.id'), nullable=False)
     discount_id = Column(Integer, ForeignKey('discounts.id'), nullable=False)
@@ -44,7 +48,7 @@ class BookDiscount(Base):
 class Book(Base):
     __tablename__ = 'books'
 
-    id = Column(SmallInteger, primary_key=True)
+    id = Column(SMALLINT, primary_key=True)
 
     title = Column(String(250), nullable=False)
     description = Column(Text)
@@ -53,8 +57,8 @@ class Book(Base):
     published_date = Column(DateTime)
     isbn = Column(String(13), unique=True)
 
-    category_id = Column(SmallInteger, ForeignKey('categories.id'))
-    publisher_id = Column(SmallInteger, ForeignKey('publishers.id'))
+    category_id = Column(SMALLINT, ForeignKey('categories.id'))
+    publisher_id = Column(SMALLINT, ForeignKey('publishers.id'))
 
     category = relationship("Category", back_populates="books")
     publisher = relationship("Publisher", back_populates="books")
@@ -73,7 +77,8 @@ class Book(Base):
 class Author(Base):
     __tablename__ = 'authors'
 
-    id = Column(SmallInteger, primary_key=True)
+    id = Column(SMALLINT, primary_key=True)
+
     name = Column(String(100), nullable=False)
     biography = Column(Text)
     birth_date = Column(DateTime)
@@ -84,7 +89,8 @@ class Author(Base):
 class Category(Base):
     __tablename__ = 'categories'
 
-    id = Column(SmallInteger, primary_key=True)
+    id = Column(SMALLINT, primary_key=True)
+
     name = Column(String(100), nullable=False)
     description = Column(Text)
 
@@ -94,7 +100,8 @@ class Category(Base):
 class Publisher(Base):
     __tablename__ = 'publishers'
 
-    id = Column(SmallInteger, primary_key=True)
+    id = Column(SMALLINT, primary_key=True)
+
     name = Column(String(100), nullable=False)
     address = Column(Text)
 
@@ -104,13 +111,15 @@ class Publisher(Base):
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(SmallInteger, primary_key=True)
+    id = Column(SMALLINT, primary_key=True)
+
     username = Column(String(100), nullable=False, unique=True)
     email = Column(String(150), nullable=False, unique=True)
     password = Column(String(150), nullable=False)
     is_active = Column(Boolean, default=False)
     created_at = Column(DateTime, default=datetime.utcnow)
     role = Column(Enum(UserRole), default=UserRole.USER)
+
     orders = relationship("Order", back_populates="user")
     reviews = relationship("Review", back_populates="user")
     addresses = relationship("Address", back_populates="user")
@@ -120,7 +129,8 @@ class User(Base):
 class OrderStatus(Base):
     __tablename__ = 'order_statuses'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(SMALLINT, primary_key=True)
+
     status = Column(Enum(order_status), nullable=False, unique=True)
     description = Column(Text)
 
@@ -130,17 +140,17 @@ class OrderStatus(Base):
 class Order(Base):
     __tablename__ = 'orders'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(SMALLINT, primary_key=True)
 
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(SMALLINT, ForeignKey('users.id'))
     total_amount = Column(Float, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
 
-    status_id = Column(Integer, ForeignKey('order_statuses.id'))
+    status_id = Column(SMALLINT, ForeignKey('order_statuses.id'))
     user = relationship("User", back_populates="orders")
     order_details = relationship("OrderDetail", back_populates="order")
-    shipping_address_id = Column(Integer, ForeignKey('addresses.id'))
-    billing_address_id = Column(Integer, ForeignKey('addresses.id'))
+    shipping_address_id = Column(SMALLINT, ForeignKey('addresses.id'))
+    billing_address_id = Column(SMALLINT, ForeignKey('addresses.id'))
     shipping_address = relationship("Address", foreign_keys=[shipping_address_id])
     billing_address = relationship("Address", foreign_keys=[billing_address_id])
     order_status = relationship("OrderStatus", back_populates="orders")
@@ -149,11 +159,11 @@ class Order(Base):
 class OrderDetail(Base):
     __tablename__ = 'order_details'
 
-    id = Column(SmallInteger, primary_key=True)
+    id = Column(SMALLINT, primary_key=True)
 
-    order_id = Column(SmallInteger, ForeignKey('orders.id'))
-    book_id = Column(SmallInteger, ForeignKey('books.id'))
-    quantity = Column(SmallInteger, nullable=False)
+    order_id = Column(SMALLINT, ForeignKey('orders.id'))
+    book_id = Column(SMALLINT, ForeignKey('books.id'))
+    quantity = Column(SMALLINT, nullable=False)
     unit_price = Column(Float, nullable=False)
 
     order = relationship("Order", back_populates="order_details")
@@ -163,11 +173,11 @@ class OrderDetail(Base):
 class Review(Base):
     __tablename__ = 'reviews'
 
-    id = Column(SmallInteger, primary_key=True)
+    id = Column(SMALLINT, primary_key=True)
 
-    user_id = Column(SmallInteger, ForeignKey('users.id'))
-    book_id = Column(SmallInteger, ForeignKey('books.id'))
-    rating = Column(SmallInteger, nullable=False)
+    user_id = Column(SMALLINT, ForeignKey('users.id'))
+    book_id = Column(SMALLINT, ForeignKey('books.id'))
+    rating = Column(SMALLINT, nullable=False)
     comment = Column(Text)
     created_at = Column(DateTime, default=datetime.utcnow)
 
@@ -182,20 +192,22 @@ class Review(Base):
 class Address(Base):
     __tablename__ = 'addresses'
 
-    id = Column(SmallInteger, primary_key=True)
-    user_id = Column(SmallInteger, ForeignKey('users.id'))
+    id = Column(SMALLINT, primary_key=True)
+
+    user_id = Column(SMALLINT, ForeignKey('users.id'))
     street = Column(String(150), nullable=False)
     city = Column(String(100), nullable=False)
     state = Column(String(100))
     country = Column(String(100), nullable=False)
     postal_code = Column(String(20), nullable=False)
+
     user = relationship("User", back_populates="addresses")
 
 
 class Discount(Base):
     __tablename__ = 'discounts'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(SMALLINT, primary_key=True)
     code = Column(String(50), unique=True, nullable=False)
     description = Column(Text)
 
