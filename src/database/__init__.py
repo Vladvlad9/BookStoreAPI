2102be09a18ac46d9352051d5458a5a18d1bd808
Vlad Paulechka <vlad9.ru@gmail.com>
2024-06-18 13:42:17 +0300

    add init

---
+++ src/database/__init__.py
@@ -0,0 +1,18 @@
+from src.database.models import *
+
+__all__ = [
+    "BookAuthor",
+    "Book",
+    "Author",
+    "Category",
+    "Publisher",
+    "User",
+    "OrderStatus",
+    "Order",
+    "OrderDetail",
+    "Review",
+    "Address",
+    "Discount",
+    "BookDiscount",
+    "Role",
+]
